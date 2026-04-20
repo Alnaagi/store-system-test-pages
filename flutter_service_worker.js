b99500c9cfc3050633d065124787359697a0a7ee
Mohamed <mohamed@local.dev>
2026-04-20 21:14:57 +0200

    Publish clearer admin login errors

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "007020f2ee3528f05f57b2742098d64f",
+"flutter_bootstrap.js": "a365e81003cd2f32e5fa09d6af829230",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "40839d46955faf253732125ab9398a1c",
 "/": "40839d46955faf253732125ab9398a1c",
-"main.dart.js": "b1a7329d3d3be05d3485e32c627aaa24",
+"main.dart.js": "9df1d247e6c86e6625659c8b01b27dee",
 "manifest.json": "dd763fa6da6cd166e64485bf9b845013",
 "version.json": "c6547d42b4ddf4684328f0fe96702253"};
 // The application shell files that are downloaded before a service worker can
