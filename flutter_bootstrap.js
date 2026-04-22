b65ab9f8f4b50bd96a63309e2aae013ecd5726c5
Mohamed <mohamed@local.dev>
2026-04-22 03:52:44 +0200

    Publish final stabilization pass

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "324754314"
+    serviceWorkerVersion: "3814492537"
   }
 });
